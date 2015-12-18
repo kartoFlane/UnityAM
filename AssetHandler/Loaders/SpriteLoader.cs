db384ba89800104ed472649ddf31981bc2acafc8
kartoFlane <tbachminski@gmail.com>
2015-12-18 17:59:44 +0100

    Added a constructor to SpriteLoader allowing to customize all its settings, not just pixels per unit.

--- AssetHandler/Loaders/SpriteLoader.cs
+++ AssetHandler/Loaders/SpriteLoader.cs
@@ -6,10 +6,16 @@ namespace AssetHandler.Loaders
 {
 	public class SpriteLoader : SynchronousAssetLoader<Sprite, SpriteParameters>
 	{
+		public readonly SpriteMeshType DefaultMeshType;
+		public readonly Vector4 DefaultBorders;
+		public readonly uint DefaultExtrude;
+		public readonly int DefaultPixelsPerUnit;
+		public readonly Vector2 DefaultPivot;
+
 		/// <summary>
 		/// Defines the type of the mesh.
 		/// </summary>
-		private SpriteMeshType meshType = SpriteMeshType.FullRect;
+		private SpriteMeshType meshType;
 
 		/// <summary>
 		/// Sprite's borders, used for nine-patches (sliced sprites)
@@ -29,17 +35,31 @@ namespace AssetHandler.Loaders
 		/// <summary>
 		/// The pivot of the sprite.
 		/// </summary>
-		private Vector2 pivot = new Vector2( 0.5f, 0.5f );
+		private Vector2 pivot = new Vector2( 0, 1 );
 
 		/// <summary>
 		/// Parameters for the TextureLoader
 		/// </summary>
 		private Texture2DParameters textureParams = null;
 
-		public SpriteLoader( IFileInfoResolver resolver, int pxPerUnit = 100 )
+		public SpriteLoader( IFileInfoResolver resolver )
+			: this( resolver, Vector2.zero, Vector4.zero )
+		{
+		}
+
+		public SpriteLoader( IFileInfoResolver resolver,
+				Vector2 pivot,
+				Vector4 borders,
+				SpriteMeshType meshType = SpriteMeshType.FullRect,
+				uint extrude = 0,
+				int pxPerUnit = 100 )
 			: base( resolver )
 		{
-			pixelsPerUnit = pxPerUnit;
+			DefaultMeshType = meshType;
+			DefaultBorders = borders;
+			DefaultExtrude = extrude;
+			DefaultPixelsPerUnit = pxPerUnit;
+			DefaultPivot = pivot;
 		}
 
 		public override Sprite LoadSync( AssetManager manager, string path, FileInfo fileHandle, SpriteParameters param )
@@ -54,7 +74,12 @@ namespace AssetHandler.Loaders
 
 		public override List<AssetDescriptor> GetDependencies( string path, FileInfo fileHandle, SpriteParameters param )
 		{
-			borders = Vector4.zero;
+			meshType = DefaultMeshType;
+			borders = DefaultBorders;
+			extrude = DefaultExtrude;
+			pixelsPerUnit = DefaultPixelsPerUnit;
+			pivot = DefaultPivot;
+
 			textureParams = null;
 
 			if ( param != null ) {
@@ -77,33 +102,33 @@ namespace AssetHandler.Loaders
 
 	public sealed class SpriteParameters : AssetLoaderParameters<Sprite>
 	{
-		public FilterMode filterMode = FilterMode.Point;
-		public TextureWrapMode wrapMode = TextureWrapMode.Clamp;
+		public FilterMode filterMode;
+		public TextureWrapMode wrapMode;
 
 		/// <summary>
 		/// Defines the type of the mesh.
 		/// </summary>
-		public SpriteMeshType meshType = SpriteMeshType.FullRect;
+		public SpriteMeshType meshType;
 
 		/// <summary>
 		/// Sprite's borders, used for nine-patches (sliced sprites)
 		/// </summary>
-		public Vector4 borders = Vector4.zero;
+		public Vector4 borders;
 
 		/// <summary>
 		/// How much empty area to leave around the sprite in the generated mesh.
 		/// </summary>
-		public uint extrude = 0;
+		public uint extrude;
 
 		/// <summary>
 		/// Pixels per game unit. This should be constant for all sprites.
 		/// </summary>
-		public int pixelsPerUnit = -1;
+		public int pixelsPerUnit;
 
 		/// <summary>
 		/// The pivot of the sprite.
 		/// </summary>
-		public Vector2 pivot = new Vector2( 0.5f, 0.5f );
+		public Vector2 pivot;
 
 		public override bool Equals( object obj )
 		{
